f6e24607f3f89a0e5fd78e81a376621647f2b2fc
Ryan-Otoole <otoolerj@gmail.com>
2022-11-28 15:40:43 -0600

    first lottery commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {LotteryToken} from "./Token.sol";
+
+contract Lottery is Ownable {
+    LotteryToken public paymentToken;
+    uint256 public closingTimeStamp;
+    uint256 public betFee;
+    uint256 public betPrice;
+    uint256 public purchaseRatio;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 _purchaseRatio,
+        uint256 _betPrice,
+        uint256 _betFee
+    ) {
+        betPrice = _betPrice;
+        paymentToken = new LotteryToken(name, symbol);
+        purchaseRatio = _purchaseRatio;
+        betFee = _betFee;
+    }
+
+    function openBets(uint256 _closingTimeStamp) external onlyOwner {
+        closingTimeStamp = _closingTimeStamp;
+    }
+}
